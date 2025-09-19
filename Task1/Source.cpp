b7013e63167a053eaeb2ae5fe0afdd4b2d7554c9
a4ivi401 <belinskiy2007@gmail.com>
2025-09-19 22:23:53 +0300

    Init project lab1.1

---
+++ Task1/Source.cpp
@@ -0,0 +1,28 @@
+int main() {
+    // short
+    const short CONST1 = -70; // Константа
+    short var1; // Описуємо змінні цілого типу short
+    var1 = 657;
+    short var2 = 0x7a; // Описуємо змінну цілого типу оператором привласнення
+
+    // int
+
+    const int CONST2 = -745; // Константа
+    int var3; // Описуємо змінну типу int
+    var3 = 20.78; // Задаємо не цілочисельне значення
+    int var4 = 228597; // Задаємо цілочисельне значення
+
+    // long
+
+    const long CONST3 = 14756987897;
+    long var5;
+    var5 = 71.8;
+    long var6 = -57;
+
+    // unsigned short
+
+    const unsigned short CONST6 = 135;
+    unsigned short var7;
+    var7 = 351362;
+    unsigned short var8 = -34;
+}
