637fffb7bfa4572348963116b5fbd7bc2325d047
a4ivi401 <belinskiy2007@gmail.com>
2025-09-20 12:24:50 +0300

    Add Task 3

---
+++ Task3/Source.cpp
@@ -0,0 +1,27 @@
+int main() {
+    int var1 = -8;
+    int var2 = var1; // Збереження знаку
+    int var3 = 890;
+    int var4 = -var3;// Заперечення знаку
+
+    int var5 = 235, var6 = -655;
+    int sum1 = var5 + var6; // Додавання
+
+    int var7 = 78, var8 = 34;
+    int dif = var7 - var8; // Віднімання
+
+    int var9 = 97, var10 = 20;
+    int multiply1 = var9 * var10; // Множення
+
+    int var11 = 15, var12 = 5;
+    int divide1 = var11 / var12; // Цілочисельне ділення 1
+
+    int var13 = 4;
+    int divide2 = var11 / var13; // Цілочисельне ділення 2
+
+    int var14 = 17, var15 = 3;
+    int remainder1 = var14 % var15; // Залишок від ділення
+
+    float var16 = 48, var17 = 7;
+    float divide3 = var16 / var17; // Ділення
+}
