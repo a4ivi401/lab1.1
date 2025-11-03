62bf1b1b1d117d4abed1b69e4b801df0bfb50145
a4ivi401 <belinskiy2007@gmail.com>
2025-11-03 23:15:49 +0200

    Обновить комментарии и форматирование в Source.cpp для задач 1 и 2

--- Task2/Source.cpp
+++ Task2/Source.cpp
@@ -1,6 +1,12 @@
-int main() {
-    const float CONST1 = 0.0000000078234;
-    float var1;
-    var1 = -0.5678e62;
-    float var2 = 3465787652.98;
-}
+// int main() {
+//     // float
+//     const float CONST1 = 0.0000000078234;
+//     float var1;
+//     var1 = -0.5678e62;
+//     float var2 = 3465787652.98;
+//
+//     //double
+//     const double CONST2 = -0.468e500;
+//     double var3;
+//     var3 = 567.67e-90;
+// }
